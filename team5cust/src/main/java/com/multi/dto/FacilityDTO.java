34b686dd6e6d5f6bea6cedc5e22a984ae59afc85
hannahjjjj <wh467925@gmail.com>
2022-11-04 00:07:40 +0900

    깃헙오류용

--- team5cust/src/main/java/com/multi/dto/FacilityDTO.java
+++ team5cust/src/main/java/com/multi/dto/FacilityDTO.java
@@ -11,7 +11,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class FacilityDTO { //깃헙 오류용
+public class FacilityDTO { //깃헙 오류
 	private int facilityid;
 	private int cateid;
 	private String facilityname;
