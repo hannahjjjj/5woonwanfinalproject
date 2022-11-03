82bab3b7a99046ac670484e65c5f1e2f1518570e
manex <manex3@nate.com>
2022-11-03 15:45:31 +0900

    commit 기본 사이트 동작작업 및 @작업

--- team5cust/src/main/java/com/multi/controller/MainController.java
+++ team5cust/src/main/java/com/multi/controller/MainController.java
@@ -11,7 +11,47 @@ public class MainController {
 		return "index";
 	}
 	
+	@RequestMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("center", "index");
+		return "redirect:/";
+	}
+		
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("center", "about");
+		return "index";
+	}
 	
+	@RequestMapping("/agent")
+	public String agent(Model model) {
+		model.addAttribute("center", "agent");
+		return "index";
+	}
+	
+	@RequestMapping("/services")
+	public String services(Model model) {
+		model.addAttribute("center", "services");
+		return "index";
+	}
+	
+	@RequestMapping("/properties")
+	public String properties(Model model) {
+		model.addAttribute("center", "properties");
+		return "index";
+	}
+	
+	@RequestMapping("/blog")
+	public String blog(Model model) {
+		model.addAttribute("center", "blog");
+		return "index";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact(Model model) {
+		model.addAttribute("center", "contact");
+		return "index";
+	}
 }
 
 
