46f028fbddf6d85d59255e81f1a2d8a722d4d7d6
hannahjjjj <wh467925@gmail.com>
2022-11-09 17:31:01 +0900

    지도

--- team5cust/src/main/java/com/multi/controller/AJAXController.java
+++ team5cust/src/main/java/com/multi/controller/AJAXController.java
@@ -3,18 +3,25 @@ package com.multi.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.multi.dto.CustDTO;
+import com.multi.dto.FacilityDTO;
 import com.multi.service.CustService;
+import com.multi.service.FacilityService;
 
 @RestController
 public class AJAXController {
 	
 	@Autowired
 	CustService custservice;
+	
+	@Autowired
+	FacilityService fservice;
 	
 	@RequestMapping("/kakkologin")
 	public String likedcnt(String id,String name,String birthday,String email,String gender,HttpSession session) {
@@ -36,5 +43,26 @@ public class AJAXController {
 			e.printStackTrace();
 		}
 		return "";
+	}
+	
+	@RequestMapping("/marker")
+	public Object marker(int facilityid) {
+		FacilityDTO fac = null;
+		JSONArray ja = new JSONArray();
+		JSONObject jo = new JSONObject();
+		try {
+			fac=fservice.get(facilityid);
+			double flat=fac.getLat();
+			double flng=fac.getLng();
+			jo.put("lat", flat);
+			jo.put("lng", flng);
+			ja.add(jo);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return ja;
 	}
 }
