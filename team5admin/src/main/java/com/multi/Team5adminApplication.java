931a7515b1360103b5100c17c4d2d3b476882d8e
manex <manex3@nate.com>
2022-11-10 09:32:51 +0900

    commit admin web site update

---
+++ team5admin/src/main/java/com/multi/Team5adminApplication.java
@@ -0,0 +1,13 @@
+package com.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5adminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5adminApplication.class, args);
+	}
+
+}
---
+++ team5admin/src/main/java/com/multi/controller/MainController.java
@@ -0,0 +1,156 @@
+package com.multi.controller;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MainController {	
+
+
+	@RequestMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("center", "index");
+		return "redirect:/";
+	}
+
+	
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("center", "about");
+		return "index";
+	}
+	@RequestMapping("/contact")
+	public String contact(Model model) {
+		model.addAttribute("center", "contact");
+		return "index";
+	}
+	
+	//login 관련
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("status", "1");
+		model.addAttribute("center", "login");
+		return "index";
+	}
+	
+//	 @RequestMapping("/loginimpl")
+//	   public String loginimpl(String id, String pwd, Model model, HttpSession session) {   
+//	      CustDTO cust = null;
+//	      try {
+//	         cust = custservice.get(id);
+//	         if(cust == null) {
+//	        	 model.addAttribute("status", "0");
+//	        	 model.addAttribute("center", "login");
+//	         } else {
+//	            if(pwd.equals(cust.getCustpwd())) {
+//	            	model.addAttribute("status", "1");
+//	               session.setAttribute("logincust", cust);
+//	               System.out.println("success");
+//	            } else {
+//	            	 model.addAttribute("status", "0");
+//	            	 model.addAttribute("center", "login");
+//	            	 }
+//	         }
+//	      } catch (Exception e) {      
+//	         e.printStackTrace();
+//	      }
+//
+//	      return "index";
+//	   }
+
+	   @RequestMapping("/logout")
+		public String logout(Model model,HttpSession session) {
+			
+			if(session != null) {
+				session.invalidate();
+			}
+			return "index";
+		}
+	   
+	 
+	// login 관련 end
+	@RequestMapping("/agent")
+	public String agent(Model model) {
+		model.addAttribute("center", "agent");
+		return "index";
+	}
+	
+	@RequestMapping("/services")
+	public String services(Model model) {
+		model.addAttribute("center", "services");
+		return "index";
+	}
+	
+
+	
+//	   @RequestMapping("/facility")
+//	   public String facility(Model model) { 
+//		   List<FacilityDTO> list = null;
+//		  try {
+//			  list=fservice.get();
+//			  model.addAttribute("facilitylist",list);
+//			  model.addAttribute("center", "facility"); 
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	      return "index";
+//	   }
+	@RequestMapping("/blog")
+	public String blog(Model model) {
+		model.addAttribute("center", "blog");
+		return "index";
+	}
+	
+	@RequestMapping("/blog-single")
+	public String blogsingle(Model model) {
+		model.addAttribute("center", "blog-single");
+		return "index";
+	}
+	
+	@RequestMapping("/search")
+	public String search(Model model) {
+		model.addAttribute("center", "search");
+		return "index";
+	}
+	@RequestMapping("/register")
+	public String register(Model model) {
+		model.addAttribute("center", "register");
+		model.addAttribute("registatus", "1");
+		return "index";
+	}
+	
+//	@RequestMapping("/registerimpl")
+//	public String registerimpl(Model model, CustDTO cust) {
+////		System.out.println(cust);
+//		try {
+//			custservice.register(cust);
+//			model.addAttribute("center","login");
+//			model.addAttribute("registatus", "1");
+//			
+//		} catch (Exception e) {
+//			model.addAttribute("center", "register");
+//			model.addAttribute("registatus", "0");
+//			e.printStackTrace();
+//		}
+//		
+//		return "index";
+//	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
