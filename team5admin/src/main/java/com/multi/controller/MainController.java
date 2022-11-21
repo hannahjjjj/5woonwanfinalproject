c8e849c355af7108436198c854fcd54d8b4366dd
Yunjinho <dbswlsgh1238@naver.com>
2022-11-21 18:13:07 +0900

    어드민 페이지 메인 화면 타임리프 분할 처리

--- team5admin/src/main/java/com/multi/controller/MainController.java
+++ team5admin/src/main/java/com/multi/controller/MainController.java
@@ -1,9 +1,5 @@
 package com.multi.controller;
 
-import javax.servlet.http.HttpSession;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
